a9cdca9f7b4da6085d3dfde3786e0c49c1789b6b
MrCarl-97 <chenhaoyuan1112@gmail.com>
2024-12-10 22:42:48 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000", "https://your-frontend-service.onrender.com"],  credentials: true }));
+// app.use(cors({ origin: ["http://localhost:3000", "https://project-3-twitter-app.onrender.com"],  credentials: true }));
+app.use(cors({
+  origin: "https://project-3-twitter-app.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
