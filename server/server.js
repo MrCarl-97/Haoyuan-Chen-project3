71d861e8159495e12e329a06596b034dfe9df6f6
MrCarl-97 <chenhaoyuan1112@gmail.com>
2024-12-10 23:09:55 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,12 +15,13 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 5000,
 })
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error(err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/posts", require("./routes/postRoutes"));
