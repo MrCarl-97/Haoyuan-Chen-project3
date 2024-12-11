6017268ca33d103750c516b91a5facbc3a140fee
MrCarl-97 <chenhaoyuan1112@gmail.com>
2024-12-10 22:27:50 -0800

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-3-twitter-app-backend.onrender.com",
 });
 
 export default api;
